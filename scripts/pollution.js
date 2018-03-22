39735a970e0587cb35d58286f0f3677cf1e7f3e6
yratanov <organium@gmail.com>
2018-03-22 10:42:54 +0700

    add polution script

---
+++ scripts/pollution.js
@@ -0,0 +1,25 @@
+module.exports = robot =>
+  robot.respond(/небо ?(.+)?/i, function(res) {
+    let url = 'http://krasnoyarsknebo.ru/share/';
+    let point;
+    let points = [
+      { title: 'свердловская', id: '1', },
+      { title: 'комсомольский', id: '2', },
+      { title: 'железнодорожников', id: '3', },
+      { title: 'копылова', id: '6', },
+      { title: 'юности', id: '13', },
+      { title: 'павлова', id: '14', },
+      { title: 'перенсона', id: '17', },
+      { title: 'ады', id: '18', },
+      { title: 'киренского', id: '21', },
+      { title: 'щорса', id: '22', },
+      { title: 'базаиха', id: '25', },
+    ];
+    let data = res.match[1];
+    point = points.find((p) => p.id === data || p.title === data.toLowerCase());
+    if (point) {
+      res.send(url + point.id);
+    } else {
+      res.send('Точка не найдена с данными ' + data)
+    }
+  });
